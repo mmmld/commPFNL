fb9c54191f14dd33a5525a973e9dd161685f1b36
mmmld <76067736+mlddemontbe@users.noreply.github.com>
2024-04-08 09:17:06 +0200

    secret key and allowed hosts

--- commpfnl/settings.py
+++ commpfnl/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,13 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lou.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
